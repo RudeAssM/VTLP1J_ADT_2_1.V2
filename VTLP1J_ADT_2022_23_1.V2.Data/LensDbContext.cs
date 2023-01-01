1056b72d9567ffbae105a545b179244278c68038
rudasm <rudasm@stud.uni-obuda.hu>
2023-01-01 22:53:18 +0200

    Dependencies startup settings and minor coding

---
+++ VTLP1J_ADT_2022_23_1.V2.Data/LensDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace VTLP1J_ADT_2022_23_1.V2.Data
+{
+    public class LensDbContext : DbContext
+    {
+        public DbSet<C>
+
+        public LensDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+        
+        
+    }
+        
+}
+
---
+++ VTLP1J_ADT_2022_23_1.V2.Endpoint/Program.cs
@@ -0,0 +1,10 @@
+using System;
+//what:D class 
+static void Program()
+{
+        
+}
+namespace VTLP1J_ADT_2022_23_1.V2.Endpoint
+{
+
+}
